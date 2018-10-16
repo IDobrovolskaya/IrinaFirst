2120d9c1b10ca199689c9a51963d9a34edc346cc
IDobrovolskaya <sun211kk@yandex.ru>
2018-10-17 00:11:33 +0300

    Commit 5 задания

--- src/main/java/HomeWork_4_present.java
+++ src/main/java/HomeWork_4_present.java
@@ -2,8 +2,9 @@ public class HomeWork_4_present {
 
     public static void main(String[] args) {
         HappyNewYearPresent array [] = new HappyNewYearPresent [2];
-        array[0] = new HappyNewYearPresent("Sweet", 100, 50);
-        array[1] = new HappyNewYearPresent("Cake", 500, 1000);
+        array [0] = new Sweet("Sweet", 100, 50, "тает во рту");
+        array [1] = new Cake ("Cake", 500, 1000,"низкокалорийный");
+
         int w = 0;
         int p = 0;
         String name = "";
@@ -14,13 +15,22 @@ public class HomeWork_4_present {
         }
         System.out.println(" Общий вес подарка " + w);
         System.out.println(" Общая стоимость подарка " + p);
-        System.out.println(" Состав подарка: " + name);
+        System.out.println(" Состав подарка: " + name +" " +w +"кг" +" " +p +"руб");
+        System.out.println ("Уникальность сладостей:" );
+        for (int i = 0; i < array.length; i++){
+            System.out.println (array [i].ingredients());
+        }
+
     }
 
     public static class HappyNewYearPresent {
         private String name;
         private int weight;
         private int price;
+
+        String ingredients (){
+            return name +" " + weight +" " +price;
+        }
 
         public HappyNewYearPresent(String name, int weight, int price) {
             this.name = name;
@@ -50,6 +60,48 @@ public class HomeWork_4_present {
 
         public void setPrice(int price) {
             this.price = price;
+        }
+    }
+
+    public static class Sweet extends HappyNewYearPresent {
+        String keySweet;
+        public Sweet(String name, int weight, int price, String keySweet) {
+            super(name, weight, price);
+            this.keySweet = keySweet;
+        }
+
+        @Override
+        String ingredients() {
+            return super.ingredients()+" " + keySweet;
+        }
+
+        public String getKeySweet() {
+            return keySweet;
+        }
+
+        public void setKeySweet(String keySweet) {
+            this.keySweet = keySweet;
+        }
+    }
+    public static class Cake extends HappyNewYearPresent {
+        String keyCake;
+
+        public Cake(String name, int weight, int price, String keyCake) {
+            super(name, weight, price);
+            this.keyCake = keyCake;
+        }
+
+        @Override
+        String ingredients() {
+            return super.ingredients()+" " +keyCake;
+        }
+
+        public String getKeyCake() {
+            return keyCake;
+        }
+
+        public void setKeyCake(String keyCake) {
+            this.keyCake = keyCake;
         }
     }
 
